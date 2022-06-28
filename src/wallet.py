ddfc339a4308755a7d7ae71486f60f269dfc053c
charudatta10 <10682378+charudatta10@users.noreply.github.com>
2022-06-28 17:14:24 +0530

    wallet code added

--- src/wallet.py
+++ src/wallet.py
@@ -1,5 +1,21 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+import uuid
+
+
 class wallet:
-    pass
+    
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+                public_exponent=65537,
+                key_size=2048,
+                )
+        self.public_key = self.private_key.public_key()
+        self.address = uuid.uuid1()
+        self.seed = 'place holder'
+
+    def save_keys(self):
+        pass
 
 if __name__ == "__main__":
-    pass
+    w = wallet()
+    print(f"Private Key : {w.private_key}\nPublic Key : {w.public_key}\nAddress : {w.address}\nSeed : {w.seed}") 
