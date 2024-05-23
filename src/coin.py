aabc0893eae1ca2c40cc3167c1cece49b6c60d6d
charudatta <152109007c@gmail.com>
2024-05-23 18:57:27 +0530

    block chain alpha release

--- src/coin.py
+++ src/coin.py
@@ -1,12 +1,37 @@
 import hashlib
+import json
+
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+message = b'This message is from me, I promise.'
 
 db=[]
 
-def get_hash(data):
+def gen_wallet():
+    key = RSA.generate(4096)
+    with open('privkey1.pem', 'wb') as f:
+        f.write(key.exportKey('PEM'))
+    with open('pubkey1.pem', 'wb') as f:
+        f.write(key.publickey().exportKey('PEM'))
+
+def gen_hash(data):
     return hashlib.sha256(str(data).encode('utf-8')).hexdigest()
 
-def get_sign():
-    pass
+def gen_sign(message, key):
+    hasher = SHA256.new(message)
+    signer = PKCS1_v1_5.new(key)
+    signature = signer.sign(hasher)
+    return signature
+
+def ver_sign(message, key, signature):
+    hasher = SHA256.new(message)
+    verifier = PKCS1_v1_5.new(key)
+    if verifier.verify(hasher, signature):
+        return True
+    else:
+        return False
 
 def get_mnemo():
     pass
@@ -14,46 +39,57 @@ def get_mnemo():
 def get_phase():
     pass
  
-def verify_add(data):
+def ver_wallet(data):
     for i in range(len(db)):
         if data in db[i]['data']:
             return True
         else:
             continue
     return False
-    
+
+
 class chain():
     def __init__(self) -> None:
         db.append({"data": "Genesis Block", "meta": "PY_DICT", "link": "0x0"})
 
     def block (self, data, meta) -> None:
-        prev_hash = get_hash(db[-1])
+        prev_hash = gen_hash(db[-1])
         return db.append({"data": data, "meta": meta, "link": prev_hash})
 
     def view_block(self):
         for i in range(len(db)):
-            print(db[i])
-
-        
-
-class trxn():
-    def __init__(self, sender, receiver, data, meta) -> None:
-        pass
+            for key,value in db[i].items():
+                print(f"key: {key}\nvalue: {value}\n---------------")
+            print("-------X-------")
     
-    def create_txn(self, meta, dest, src, data):
-        self.check_validity(meta, dest, src, data)
-        return {"meta": meta, "to": dest, "from": src, "data": data}
+    def save_chain(self):
+        with open('block_chain.json', 'w') as fp:
+            json.dump(db, fp, indent=4)
+
+    def load_chain(self):
+        with open('block_chain.json', 'r') as fp:
+            db = json.load(fp)
+
+
+class trxn():  
     
-    def check_validity(self, meta,  dest, src, data):
+    def ver_trxn(self, meta,  dest, src, data):
         match meta:
-            case "wallet_creation": verify_add(data) # check if address exist search in data in block chain
-                # and verify sender with digital signature
-                
-                
+            case "wallet_creation": 
+                if(not ver_wallet(data)): # check if address exist search in data in block chain
+                    print("wallet needs to be created")
+                    return True
+                else:
+                    print("wallet already exist")
+                    return False
+                # and verify sender with digital signature        
             case "money": """ verify sender, verify sender has money, verify reciter, do transection"""
+
+
 class wallet():
     def __init__(self) -> None:
         pass
+
 
 class mine():
     def __init__(self) -> None:
@@ -62,17 +98,23 @@ class mine():
 
 if __name__ == "__main__":
     c1 = chain()
-    print(db)
     c1.view_block()
-    print(db)
     c1.block("block 1", "PY_DICT")
-    print(db)
     c1.view_block()
-    print(db)
-    c1.block("block 2", "PY_DICT")
-    print(db)
+    #gen_wallet()
+    with open('pubkey1.pem', 'rb') as f:
+        key = RSA.importKey(f.read())
+        key1 = f.read()
+    t1 = trxn()
+    if t1.ver_trxn("wallet_creation", "", "",str(key.exportKey())):
+        c1.block(str(key.exportKey()),"GEN_WALLET")
     c1.view_block()
-    print(db)
-    
-    print(db[0]['data'])
-    print(verify_add('block 1'))
+    if t1.ver_trxn("wallet_creation", "", "",str(key.exportKey())):
+        c1.block(str(key.exportKey()),"GEN_WALLET")
+    c1.view_block()
+    print(f"---Save Block chain ------")
+    c1.save_chain()
+    print(f"---Load Block chain ------")
+    c1.load_chain()
+    print(f"---Verify Block chain ------")
+    c1.view_block()
