82a4535f8654273cb1ce3d1cdb2678f32df252d5
charudatta <152109007c@gmail.com>
2024-06-26 21:59:13 +0530

    modified txn class

--- src/trxn.py
+++ src/trxn.py
@@ -1,42 +1,38 @@
 
 
 class Trxn():  
-    def __init__(self, block) -> None:
-        self.block = block
+    def __init__(self, src, dst, sub, sgn, box) -> None:
+        self.src = src # source
+        self.dst = dst # destination
+        self.sub = sub  # subject / type
+        self.sgn = sgn # signature 
+        self.box = box # content amount 
 
-    def create_trxn(self):
-        pass
-    def get_trxn(self):
-        pass
-    def add_trxn(self):
+class VM():
+    def __init__(self, db, tn: Trxn) -> None:
+        self.db = db
+        self.tn = tn
+
+    def txn_wallet_gen(self):
+        print(self.tn.box)
         pass
 
-    def ver_wallet(self):
-        """list_val = [value for self.block["address"] in db.["data"]["address"]]# check if address exist search in data in block chain
-                    print("wallet does not exist!")
-                    return True
-                else:
-                    print("wallet already exist!")
-                    return False"""
-
-    def ver_money(self):
+    def txn_dao(self):
         pass
-        """block = t_block[len(self.db) + 1]
-        print(block)
-        match block['meta']:
-            case "wallet_creation": 
-                if(not wallet().ver_wallet(self.db, block['data'])): # check if address exist search in data in block chain
-                    print("wallet does not exist!")
-                    return True
-                else:
-                    print("wallet already exist!")
-                    return False
-                # and verify sender with digital signature        
-            case "money": 
-                if wallet().ver_wallet(self.db, block['data']['src']):
-                    if wallet().ver_wallet(self.db, block['data']['dest']):
-                        amt = wallet().ver_wallet_amt(self.db, block['data'])
-                        return True if amt-block['data']['amt'] > 0 else False
-                
-                
-                #verify sender, verify sender has money, verify receiver, do transaction"""
+
+    def txn_nft(self):
+        pass
+
+    def txn_coin(self):
+        pass
+
+    def txn_token(self):
+        pass
+
+    def txn_smart_ctr(self):
+        pass
+
+if __name__ == "__main__":
+    t1 = Trxn(1,2,3,4,5)
+    v1 = VM(1,t1)
+    v1.txn_wallet_gen()
