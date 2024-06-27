48522bbfdef4bcc0f60d8bc88d4a6083712c0eca
charudatta <152109007c@gmail.com>
2024-06-27 16:52:37 +0530

    added comments

--- src/trxn.py
+++ src/trxn.py
@@ -15,12 +15,29 @@ class VM():
 
     def txn_wallet_gen(self):
         print(self.tn.box)
+        # 1. generate wallet
+        # 2. generate wallet transaction
+        # 3. verify wallet unique 
+        # 4. add transaction to block
+        # 5. add block to chain
         pass
 
     def txn_dao(self):
         pass
 
     def txn_nft(self):
+        # 1. generate nft
+        # 2. generate nft transaction
+        # 3. verify nft unique 
+        # 4. add transaction to block
+        # 5. add block to chain
+        # 6. NFT use:
+        # 6.1. search if nft exist 
+        # 6.2. if exist increase hit count of nft state
+        # 6.3. create money transaction from hit count to owner of NFT
+        # 6.4. execute transaction
+        # 6.5. add transaction to block
+        # 6.6. add block to chain
         pass
 
     def txn_coin(self):
